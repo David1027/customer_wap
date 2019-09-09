90e97029b2f46f41384489de0d1438d4f54e9156
admin <linhuali@shoestp.com>
2019-09-09 14:33:27 +0800

    登录页提交

---
+++ components/agency-list.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <!-- 中介公司列表 -->
+    <div class="agency_list" v-if="agencyList.length > 0">
+      <div v-for="(item,index) in agencyList" :key="index" class="agency_item" @click="skip()">
+        <div class="title">
+          <p class="company_name">
+            中介公司名称
+          </p>
+          <p class="contract_name">
+            签约人名称
+          </p>
+        </div>
+        <div class="status">
+          <div class="status_item item1">
+            登记: <span class="number">
+            11
+          </span>
+          </div>
+          <div class="status_item item2">
+            签约: <span class="number">
+            22
+          </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="noData" v-else>
+      暂无中介公司数据...
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AgencyList',
+  props: {
+    agencyList: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    skip(agencyId) {
+      this.$router.push({ path: '/m/agentMange', query: { agencyId: agencyId }})
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/css/_mixin.scss";
+
+.agency_list{
+  width: pxToRem(690);
+  margin: 0 auto;
+  box-shadow: 0px 0px 16px 0px rgba(50, 50, 50, 0.29);
+  border-radius: pxToRem(20);
+  padding: pxToRem(10) 0;
+
+  .agency_item{
+    height: pxToRem(165);
+    padding: pxToRem(30) 0;
+    margin: 0 pxToRem(30);
+    border-bottom: pxToRem(1) solid #dddddd;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: space-evenly;
+
+    &:last-of-type{
+      border-bottom: pxToRem(1) solid rgba(221, 221, 221, 0);
+    }
+
+    .title{
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
+      .company_name{
+        @include font-dpr(30);
+        color: #333333;
+        line-height: pxToRem(40);
+      }
+      .contract_name{
+        @include font-dpr(24);
+        color: #999999;
+      }
+    }
+    .status{
+      width: 100%;
+      display: flex;
+      justify-content: flex-start;
+
+      .status_item{
+        @include font-dpr(24);
+        color: #666666;
+        margin-right: pxToRem(40);
+        .number{
+          margin-left: pxToRem(10);
+        }
+
+        &.item1{
+          .number{
+            color: #008fd7;
+          }
+        }
+        &.item2{
+          .number{
+            color: #e62129;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/m/index.vue
+++ pages/m/index.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="home">
-    <div class="header">
-
+    <div class="header back_box">
+      <div class="img_cover logo">
+        <img src="~/assets/images/logo.jpg" alt="">
+      </div>
     </div>
+    <div class="header_nav">
+      <div class="nav_item" @click="navShow = 0">
+        <p class="number">15</p>
+        <p class="nav_name">中介公司</p>
+        <span class="nav_wire" :class="navShow === 0? 'item1' : ''"></span>
+      </div>
+      <div class="nav_item" @click="navShow = 1">
+        <p class="number">15</p>
+        <p class="nav_name">客户数</p>
+        <span class="nav_wire" :class="navShow === 1? 'item2' : ''"></span>
+      </div>
+    </div>
+    <agency-list v-show="navShow === 0"></agency-list>
+    <client-list v-show="navShow === 1"></client-list>
   </div>
 </template>
 
 <script>
+import AgencyList from '~/components/agency-list'
+import ClientList from '~/components/client-list'
 export default {
+  components: {
+    AgencyList,
+    ClientList
+  },
   data() {
-    return {}
+    return {
+      navShow: 0
+    }
   },
   methods: {
 
@@ -21,5 +45,56 @@ export default {
 @import "~/assets/css/_mixin.scss";
 .home{
 
+  .header{
+    width: 100%;
+    height: pxToRem(174);
+    background-image: url("~assets/images/banner-back.png");
+    background-size: pxToRem(524);
+    background-position: pxToRem(363) pxToRem(-250);
+    display: flex;
+    align-items: center;
+    border-bottom: solid pxToRem(1) #d2d2d2;
+
+    .logo{
+      width: pxToRem(199);
+      height: pxToRem(82);
+      margin-left: pxToRem(35);
+    }
+  }
+  .header_nav{
+    width: 100%;
+    padding: pxToRem(70) 0;
+    display: flex;
+    justify-content: space-between;
+
+    .nav_item{
+      width: 50%;
+      text-align: center;
+      .number{
+        font-family: MicrosoftYaHei-Bold;
+        @include font-dpr(54);
+        color: #333333;
+        font-weight: bolder;
+      }
+      .nav_name{
+        @include font-dpr(24);
+        color: #999999;
+        line-height: pxToRem(50);
+      }
+      .nav_wire{
+        display: inline-block;
+        width: pxToRem(78);
+        height: pxToRem(14);
+        border-radius: pxToRem(7);
+        background-color: rgba(255, 255, 255, 0);
+        &.item1{
+          background-image: linear-gradient(90deg, #0ca5db 0%, #a5cc2a 100%);
+        }
+        &.item2{
+          background-image: linear-gradient(90deg, #ef7c4f 0%, #ea479d 100%);
+        }
+      }
+    }
+  }
 }
 </style>
